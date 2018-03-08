65b4e4b4b1f0726080d41ed576e2395638cf768b
Boekel,Kelvin K. van <kelvinvanboekel@gmail.com>
2018-03-08 11:26:02 +0100

    workshop resources

---
+++ GTO4/Assets/Scripts/ResourceController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceController : MonoBehaviour
+{
+    public UIScript uiScript;
+    private int gold = 1000;
+    private int wood = 1000;
+
+    public void addGold()
+    {
+        gold += 50;
+        uiScript.changeGold(gold);
+    }
+
+    public void addWood()
+    {
+        wood += 50;
+        uiScript.changeWood(wood);
+    }
+
+    public void buySoldier()
+    {
+        gold -= 100;
+        wood -= 100;
+        uiScript.changeWood(wood);
+        uiScript.changeGold(gold);
+    }
+
+    
+}
---
+++ GTO4/Assets/Scripts/UIScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    public Text woodtxt;
+    public Text goldtxt;
+
+    public void changeGold(int gold)
+    {
+        goldtxt.text = gold.ToString();
+    }
+
+    public void changeWood(int wood)
+    {
+        woodtxt.text = wood.ToString();
+    }
+}
